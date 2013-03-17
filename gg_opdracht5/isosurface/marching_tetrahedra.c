dfe8e4802772c95aee533990ea9c9e79ee3926f8
Tomaat <y.galama@hotmail.com>
2013-03-17 17:38:46 +0100

    YAY

--- gg_opdracht5/isosurface/marching_tetrahedra.c
+++ gg_opdracht5/isosurface/marching_tetrahedra.c
@@ -58,28 +58,33 @@ generate_tetrahedron_triangles(triangle *triangles, unsigned char isovalue, cell
 	int bitvalue[4];
 	// TODO wat is v0?? de index? de waarde van de vertices?
 	// set bitvalues of vertices:
-	if (((int)c.value[v0]) <= isovalue) bitvalue[0] = 0; else bitvalue[0] = 1;
-	if (((int)c.value[v1]) <= isovalue) bitvalue[1] = 0; else bitvalue[1] = 1;
-	if (((int)c.value[v2]) <= isovalue) bitvalue[2] = 0; else bitvalue[2] = 1;
-	if (((int)c.value[v3]) <= isovalue) bitvalue[3] = 0; else bitvalue[3] = 1;
+	if (((int)c.value[v3]) <= isovalue) bitvalue[0] = 0; else bitvalue[0] = 1;
+	if (((int)c.value[v2]) <= isovalue) bitvalue[1] = 0; else bitvalue[1] = 1;
+	if (((int)c.value[v1]) <= isovalue) bitvalue[2] = 0; else bitvalue[2] = 1;
+	if (((int)c.value[v0]) <= isovalue) bitvalue[3] = 0; else bitvalue[3] = 1;
 //	printf("%d -> (%d,%d,%d,%d) = (%d,%d,%d,%d) =>",isovalue,(int)c.value[v0],(int)c.value[v1],(int)c.value[v2],(int)c.value[v3],bitvalue[0],bitvalue[1],bitvalue[2],bitvalue[3]);
 	/* Cases of 1 triangle:*/
-	// case of 0001 or 1110
+	// case of 0001 or 1110 ERRORv3
 	if(bitvalue[1] == bitvalue[2] && bitvalue[2] == bitvalue[0] && bitvalue[0] != bitvalue[3])
     {//printf("0001|1110\n");
+/*    printf("0001: %d <-%d-> %d | %d <-%d-> %d | %d <-%d-> %d\n",
+    (int)c.value[v0],isovalue,(int)c.value[v1],
+    (int)c.value[v0],isovalue,(int)c.value[v2],
+    (int)c.value[v0],isovalue,(int)c.value[v3]
+    );*/
         triangles[0].p[0] = interpolate_points(isovalue, c.p[v0], c.p[v1], c.value[v0], c.value[v1]);
         triangles[0].p[1] = interpolate_points(isovalue, c.p[v0], c.p[v2], c.value[v0], c.value[v2]);
         triangles[0].p[2] = interpolate_points(isovalue, c.p[v0], c.p[v3], c.value[v0], c.value[v3]);
-printf("%d -> (%d,%d,%d,%d) = (%d,%d,%d,%d) => (%.0f,%.0f,%.0f):(%.0f,%.0f,%.0f):(%.0f,%.0f,%.0f)\n",isovalue,
+/*printf("%d -> (%d,%d,%d,%d) = (%d,%d,%d,%d) => (%.0f,%.0f,%.0f):(%.0f,%.0f,%.0f):(%.0f,%.0f,%.0f)\n",isovalue,
 (int)c.value[v0],(int)c.value[v1],(int)c.value[v2],(int)c.value[v3],
 bitvalue[0],bitvalue[1],bitvalue[2],bitvalue[3],
 triangles[0].p[0].x,triangles[0].p[0].y,triangles[0].p[0].z,
 triangles[0].p[1].x,triangles[0].p[1].y,triangles[0].p[1].z,
 triangles[0].p[2].x,triangles[0].p[2].y,triangles[0].p[2].z
-);
+);*/
         return 1;
     }
-	// case of 0010 or 1101
+	// case of 0010 or 1101 ERRORv2
 	if(bitvalue[0] == bitvalue[1] && bitvalue[1] == bitvalue[3] && bitvalue[0] != bitvalue[2])
     {//printf("0010|1101\n");
         triangles[0].p[0] = interpolate_points(isovalue, c.p[v1], c.p[v0], c.value[v1], c.value[v0]);
@@ -87,7 +92,7 @@ triangles[0].p[2].x,triangles[0].p[2].y,triangles[0].p[2].z
         triangles[0].p[2] = interpolate_points(isovalue, c.p[v1], c.p[v3], c.value[v1], c.value[v3]);
         return 1;
     }
-	// case of 0100 or 1011
+	// case of 0100 or 1011 ERRORv1
 	if(bitvalue[0] == bitvalue[2] && bitvalue[2] == bitvalue[3] && bitvalue[0] != bitvalue[1])
     {//printf("0100|1011\n");
         triangles[0].p[0] = interpolate_points(isovalue, c.p[v2], c.p[v0], c.value[v2], c.value[v0]);
@@ -108,11 +113,11 @@ triangles[0].p[2].x,triangles[0].p[2].y,triangles[0].p[2].z
 	// case of 0011 or 1100
     if(bitvalue[0] == bitvalue[1] && bitvalue[2] == bitvalue[3] && bitvalue[0] != bitvalue[2])
     {//printf("0011|1100\n");
-        triangles[0].p[0] = interpolate_points(isovalue, c.p[v0], c.p[v1], c.value[v0], c.value[v1]);
+        triangles[0].p[0] = interpolate_points(isovalue, c.p[v2], c.p[v1], c.value[v2], c.value[v1]);
         triangles[0].p[1] = interpolate_points(isovalue, c.p[v0], c.p[v2], c.value[v0], c.value[v2]);
         triangles[0].p[2] = interpolate_points(isovalue, c.p[v0], c.p[v3], c.value[v0], c.value[v3]);
 
-        triangles[1].p[0] = interpolate_points(isovalue, c.p[v1], c.p[v0], c.value[v1], c.value[v0]);
+        triangles[1].p[0] = interpolate_points(isovalue, c.p[v3], c.p[v0], c.value[v3], c.value[v0]);
         triangles[1].p[1] = interpolate_points(isovalue, c.p[v1], c.p[v2], c.value[v1], c.value[v2]);
         triangles[1].p[2] = interpolate_points(isovalue, c.p[v1], c.p[v3], c.value[v1], c.value[v3]);
         return 2;
@@ -121,10 +126,10 @@ triangles[0].p[2].x,triangles[0].p[2].y,triangles[0].p[2].z
     if(bitvalue[0] == bitvalue[2] && bitvalue[1] == bitvalue[3] && bitvalue[0] != bitvalue[1])
     {//printf("0101|1010\n");
         triangles[0].p[0] = interpolate_points(isovalue, c.p[v0], c.p[v1], c.value[v0], c.value[v1]);
-        triangles[0].p[1] = interpolate_points(isovalue, c.p[v0], c.p[v2], c.value[v0], c.value[v2]);
+        triangles[0].p[1] = interpolate_points(isovalue, c.p[v1], c.p[v2], c.value[v1], c.value[v2]);
         triangles[0].p[2] = interpolate_points(isovalue, c.p[v0], c.p[v3], c.value[v0], c.value[v3]);
 
-        triangles[1].p[0] = interpolate_points(isovalue, c.p[v2], c.p[v0], c.value[v2], c.value[v0]);
+        triangles[1].p[0] = interpolate_points(isovalue, c.p[v3], c.p[v0], c.value[v3], c.value[v0]);
         triangles[1].p[1] = interpolate_points(isovalue, c.p[v2], c.p[v1], c.value[v2], c.value[v1]);
         triangles[1].p[2] = interpolate_points(isovalue, c.p[v2], c.p[v3], c.value[v2], c.value[v3]);
         return 2;
@@ -134,9 +139,9 @@ triangles[0].p[2].x,triangles[0].p[2].y,triangles[0].p[2].z
     {//printf("1001|0110\n");
         triangles[0].p[0] = interpolate_points(isovalue, c.p[v0], c.p[v1], c.value[v0], c.value[v1]);
         triangles[0].p[1] = interpolate_points(isovalue, c.p[v0], c.p[v2], c.value[v0], c.value[v2]);
-        triangles[0].p[2] = interpolate_points(isovalue, c.p[v0], c.p[v3], c.value[v0], c.value[v3]);
+        triangles[0].p[2] = interpolate_points(isovalue, c.p[v1], c.p[v3], c.value[v1], c.value[v3]);
 
-        triangles[1].p[0] = interpolate_points(isovalue, c.p[v3], c.p[v0], c.value[v3], c.value[v0]);
+        triangles[1].p[0] = interpolate_points(isovalue, c.p[v2], c.p[v0], c.value[v2], c.value[v0]);
         triangles[1].p[1] = interpolate_points(isovalue, c.p[v3], c.p[v1], c.value[v3], c.value[v1]);
         triangles[1].p[2] = interpolate_points(isovalue, c.p[v3], c.p[v2], c.value[v3], c.value[v2]);
         return 2;
